08c05c641bd73436aebefe6db56ea1056789c131
Afnan <63553675+AfnanElhussini@users.noreply.github.com>
2023-03-10 17:49:44 +0200

    Added Casting & Type Conversions

---
+++ Casting/Boxing & unboxing/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Boxing___unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // boxing 
+            int number = 10; //value Type
+            object obj = number; // boxing
+            // object is on heap and heap is larger  and number is on stack
+
+            // unboxing (Explict casting)
+           
+            int number2 = (int)obj; // unboxing
+
+            // convert from string to numerical
+            // 1). int.Parse(), 2). Convert.ToInt32()
+            // int intValue = int.Parse(stringValue);  // overFlow exception
+            string str = "100";
+            int number3 = Convert.ToInt32(str);
+
+
+            string stringValue = "9999999999999";
+            if (int.TryParse(stringValue, out int intValue))
+            {
+                Console.WriteLine(intValue);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number does not fit integer");
+            }
+
+            // Convert Class
+            var number12 = 255;
+            //int is 4 bytes
+            var bytes = BitConverter.GetBytes(number12);
+            foreach(var b in bytes)
+            {
+                Console.WriteLine(b);
+                var binary = Convert.ToString(b, 2).PadLeft(8, '0');
+                Console.WriteLine(binary);
+            }
+
+
+            // from string to binary
+            var name = "Afnan";
+            char[] letters = name.ToCharArray();
+            foreach(var b in letters)
+            {
+                int ascii = Convert.ToInt32(b);
+               
+            }
+        }
+    }
+}
---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit Casting (automatically) - converting a smaller type to a larger type size
+
+            int numberInt = 100;
+            long numberLong = numberInt;
+
+            //-------------------------------
+            long nL = 1000;
+            //  int nI= nl; // Compilation error can not implicitly convert type 'long' to 'int'. An explicit conversion exists (are you missing a cast?)
+
+            // we can use Explict casting to convert a larger type to a smaller type size
+            
+            if (nL <= long.MaxValue)
+            {
+                int nI = (int)nL;
+
+            }
+            //another example
+            double d = 1234.8;
+            int i = (int)d;
+            Console.WriteLine(i);
+        }
+    }
+}
