ea7975379b0dc6b0c42feca91757a18cc29df92b
Afnan <63553675+AfnanElhussini@users.noreply.github.com>
2023-03-11 22:58:02 +0200

    OOP Classes & Objects
    OOP definition, Classes Vs. Objects, classes Modifier, access modifier, died and constant

---
+++ Field & Constant/Field & Constant/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field___Constant
+{
+    // Access modifier class modifiers >> public , internal is default
+    // class block >> { }
+    // class name >> Employee
+    // class body >> { }
+    // class members >> fields, properties, methods, events, constructors, indexers, operators, finalizers, nested typess
+    public class Employee
+    {
+        //Fields 
+        // Aceess modifier >> public, private, protected, internal, protected internal
+
+        public string fName;
+        public string lName;
+        public double wage;
+        public double loggedHours;
+        //constant 
+        public const double TAX = 0.03;
+
+    }
+}
---
+++ Field & Constant/Field & Constant/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Field___Constant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Field & Constant 
+            // Field: A field is a variable that is declared inside a class or struct.
+            // Constant: A constant is a field that is declared with the const modifier.
+
+            // Object syntax
+            // objectName.fieldName = value;
+            Employee e1 = new Employee();
+            Console.WriteLine("First Employee");
+
+            Console.WriteLine("First Name");
+            e1.fName = Console.ReadLine();
+
+
+
+
+            const  double TAX= 0.03;
+            
+            Console.WriteLine("Last Name");
+            e1.lName= Console.ReadLine();
+
+            Console.Write("wage");
+            e1.wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("logged Hours");
+            var loggedHours= Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Second Employee");
+            Employee e2 = new Employee();
+            Console.WriteLine("First Name");
+            e2.fName = Console.ReadLine();
+
+            Console.WriteLine("Last Name");
+            e2.lName = Console.ReadLine();
+
+            Console.Write("wage");
+            e2.wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("logged Hours");
+            var loggedHours2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("First Employee Data");
+          var netSalary =e1. wage * loggedHours - (e1.wage * loggedHours * Employee.TAX);
+            Console.WriteLine($"First Name : {e1.fName}");
+            Console.WriteLine($"Last Name : {e1.lName}");
+            Console.WriteLine($"Wage: {e1.wage}");
+            Console.WriteLine($"Logged Hours: {loggedHours}");
+            Console.WriteLine($"Net Salary: {netSalary}");
+
+
+            Console.WriteLine("Second Employee Data");
+            double netSalary2 = e2.wage * loggedHours2 - (e2.wage * loggedHours2 * Employee.TAX);
+            Console.WriteLine($"First Name : {e2.fName}");
+            Console.WriteLine($"Last Name : {e2.lName}");
+            Console.WriteLine($"Wage: {e2.wage}");
+            Console.WriteLine($"Logged Hours: {loggedHours2}");
+            Console.WriteLine($"Net Salary: {netSalary2}");
+            Console.WriteLine(
+                "Press any key to exit"
+                               );
+
+        }
+    }
+}
